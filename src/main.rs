41a72842686ecbb91b9278512925db6e9abf6c9d
somdipdatta <somdip@alumni.princton.edu>
2023-08-27 18:54:05 -0400

    try smallRng and pre allocate vector

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use rand::thread_rng;
-use rand_distr::{Normal, Distribution};
+use rand_distr::Distribution;
 use std::time::Instant;
+use statrs::distribution::Normal;
+use rand::SeedableRng;
 
 
 fn sim() {
-    let mut rng = thread_rng();
+    let mut rng = rand::rngs::SmallRng::from_entropy();
     let normal = Normal::new(1.0, 1.0).unwrap();
 
-    let num_paths: usize = 100000;
-    let num_steps: usize = 1000;
+    let num_paths: usize = 200000;
+    let num_steps: usize = 700;
 
-    // This takes about 20 secs. Next, how to collect into pre-allocated array?
+    // Initialize the vector
+    let mut _v: Vec<f64> = Vec::with_capacity(num_paths);
+
+    // This takes about 0.9 to 1.1 secs. which matches numpy with same parameters, and 'out' option.
     for _step in 1..num_steps {
-        let _v: Vec<f32> = normal.sample_iter(&mut rng).take(num_paths).collect();
+        _v = normal.sample_iter(&mut rng).take(num_paths).collect();
     }
 }
 
